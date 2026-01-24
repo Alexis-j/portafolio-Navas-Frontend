176ffa1f8e0c4534597d8557859f4c8f3e6d0e16
alexis jimenez castillo <alexis.jcastillo@outlook.com>
2026-01-24 21:40:17 +0100

    preparando deploy

--- src/services/api.js
+++ src/services/api.js
@@ -1,18 +1,10 @@
 import axios from "axios";
 
-// 🔹 Base URL según tu .env
-// Para local: REACT_APP_API_URL=http://localhost:5000
-// Para ngrok: REACT_APP_API_URL=https://tu-subdominio.ngrok-free.dev
-const BASE_URL = process.env.REACT_APP_API_URL;
+const BASE_URL = process.env.REACT_APP_API_URL; // ahora apunta al backend en Railway
 
-// 🔹 Instancia principal de Axios
 const api = axios.create({
-  baseURL: `${BASE_URL}/api`,
-  headers: {
-    "ngrok-skip-browser-warning": "true",
-  },
+  baseURL: `${BASE_URL}/api`, // tus endpoints siguen /api
 });
-
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
